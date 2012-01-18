0dab035f13ee8f953aa8e4aed1e20554c54b25ad
Eugeniy Kalinin <burump@gmail.com>
2012-01-18 13:41:08 -0800

    Added a required resource to the reservation

--- test/resources_ut.js
+++ test/resources_ut.js
@@ -9,4 +9,12 @@ $(function() {
         notEqual(res.validate({ name: '' }), undefined);
         notEqual(res.validate({}), undefined);
     });
+
+    test('unique()', function() {
+        Hoard.resources.add({ name: 'Trout Blade' });
+        var res = new Hoard.models.Resource;
+
+        ok(res.unique('Trout'));
+        ok( ! res.unique('Trout Blade'));
+    });
 });
