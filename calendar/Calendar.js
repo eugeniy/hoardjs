19f89d21ac4662fb4982d3c3a5a2491e48a17661
Farrin Reid <blakmatrix@gmail.com>
2012-02-07 15:25:08 +0000

    models, collections file for testing, and currently erroing with toJSON not a func of something

--- calendar/Calendar.js
+++ calendar/Calendar.js
@@ -14,5 +14,136 @@
 	    }
 	});
 
+	window.Calendars = Backbone.Collection.extend({
+		model: Calendar,
+		url: 'calendars.json'
+	});
+    
+    //====================== DAY VIEW====================
+	window.DayCalendarView = window.CalendarView.extend({
+		
+	});
+
+	window.DayView = Backbone.View.extend({
+		tagName: 'section',
+		className: 'day',
+
+		initialize: function(){
+			_.bindAll(this, 'render');
+			this.template = _.template($('#day-template').html());
+			this.collection.bind('reset', this.render);
+		},
+
+		render: function(){
+			var $calendars,
+			    collection = this.collection;
+			$(this.el).html(this.template({}));
+			$calendars = this.$('.calendars1');
+			collection.each(function(calendars){
+				var view = new DayCalendarView({
+					model: Calendar,
+					collection: collection
+				});
+				$calendars.append(view.render().el);
+			});
+			return this;
+		}
+
+	});
+	//====================== WEEK VIEW=======================
+    window.WeekCalendarView = window.DayCalendarView.extend({
+		
+	});
+
+	window.WeekView = Backbone.View.extend({
+		tagName: 'section',
+		className: 'week',
+
+		initialize: function(){
+			_.bindAll(this, 'render');
+			this.template = _.template($('#week-template').html());
+			this.collection.bind('reset', this.render);
+		},
+
+		render: function(){
+			var $calendars,
+			    collection = this.collection;
+			$(this.el).html(this.template({}));
+			$calendars = this.$('.calendars2');
+			collection.each(function(calendars){
+				var view = new WeekCalendarView({
+					model: Calendar,
+					collection: collection
+				});
+				$calendars.append(view.render().el);
+			});
+			return this;
+		}
+
+	});
+    //====================== MONTH VIEW =======================
+	window.MonthCalendarView = window.WeekCalendarView.extend({
+		
+	});
+
+	window.MonthView = Backbone.View.extend({
+		tagName: 'section',
+		className: 'month',
+
+		initialize: function(){
+			_.bindAll(this, 'render');
+			this.template = _.template($('#month-template').html());
+			this.collection.bind('reset', this.render);
+		},
+
+		render: function(){
+			var $calendars,
+			    collection = this.collection;
+			$(this.el).html(this.template({}));
+			$calendars = this.$('.calendars3');
+			collection.each(function(calendars){
+				var view = new MonthCalendarView({
+					model: Calendar,
+					collection: collection
+				});
+				$calendars.append(view.render().el);
+			});
+			return this;
+		}
+
+	});
+    
+    //====================== AGENDA VIEW =======================
+	window.AgendaCalendarView = window.WeekCalendarView.extend({
+		
+	});
+
+	window.AgendaView = Backbone.View.extend({
+		tagName: 'section',
+		className: 'agenda',
+
+		initialize: function(){
+			_.bindAll(this, 'render');
+			this.template = _.template($('#agenda-template').html());
+			this.collection.bind('reset', this.render);
+		},
+
+		render: function(){
+			var $calendars,
+			    collection = this.collection;
+			$(this.el).html(this.template({}));
+			$calendars = this.$('.calendars4');
+			collection.each(function(calendars){
+				var view = new AgendaCalendarView({
+					model: Calendar,
+					collection: collection
+				});
+				$calendars.append(view.render().el);
+			});
+			return this;
+		}
+
+	});
+
 
 })(jQuery);
